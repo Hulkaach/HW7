c1c315039621831866f1ac07ead1ab9af4954b12
DESKTOP-QQACB3T\Chester <zolinanata2006@mail.ru>
2023-11-28 22:10:51 +0300

    1

--- Task 2/src/Main.java
+++ Task 2/src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
+        int countPeople = 100;
+        for (int i = 2; i <= countPeople; i++) {
+            System.out.println("Уволен айтишник с id "+i);
+            i++;
+        }
         //todo Тут увольняем айтишников
     }
 }
