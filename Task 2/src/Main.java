6f68747598dac13c7587df3dbc3bdf2d42030f63
DESKTOP-QQACB3T\Chester <zolinanata2006@mail.ru>
2023-11-28 22:25:05 +0300

    2

--- Task 2/src/Main.java
+++ Task 2/src/Main.java
@@ -1,9 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        int countPeople = 100;
-        for (int i = 2; i <= countPeople; i++) {
-            System.out.println("Уволен айтишник с id "+i);
-            i++;
+        for (int i = 2; i <= 100; i = i + 2) {
+            System.out.println("Уволен айтишник с id " + i);
+        }
+
+        int i = 2;
+        while (i <= 100) {
+            System.out.println("Уволен айтишник с id " + i);
+            i = i + 2;
         }
         //todo Тут увольняем айтишников
     }
